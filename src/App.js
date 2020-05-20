f12c3ac3a3e2e8b986a3d891fed67ba07e299ec0
Kate Tsareva <katetsareva3012@mail.ru>
2020-05-20 18:37:36 +0300

    chore: lesson react-element

--- src/App.js
+++ src/App.js
@@ -1,35 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import * as lesson from './number';
 
-const flag= true, text = 'lesson';
+const element = (<div>Hi! <b>16</b> lesson</div>);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{fontSize: 36-4}}>
-          count*length = {lesson.count * lesson.length}
-        </p>
-        <p>
-          Осталось уроков {lesson.all - lesson.num}
-        </p>
-        <p>
-          {lesson.num < 30 ? 'Это только начало' : 'Еще не много'}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export { element };
--- src/number.js
+++
@@ -1,3 +0,0 @@
-const num = 15, all = 44;
-const count = 5, length = 25;
-export {num, all, count, length};
