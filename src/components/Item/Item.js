aa06a7c751ff726b4e5f89f10b67319148fa3a79
Kate Tsareva <katetsareva3012@mail.ru>
2020-06-27 18:49:27 +0300

    chore: Lifecycle Methods demo

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,23 +4,59 @@ import classnames from 'classnames';
 import del from './delete.svg';
 import styles from './Item.module.css';
 
-const Item = ({value, isDone, id, onClickDone, onClickDelete}) => (
-  <div className={
-    classnames({
-      [styles.item]: true,
-      [styles.done]: isDone,
-    })
-  }>
-    <label>
-      <input type="checkbox" name="itemList" onClick={() => onClickDone(id)}/>
-      <span className={styles.check}></span>
-      {value}
-    </label>
-    <div className={styles.delete} onClick={()=> onClickDelete(id)}>
-      <img src={del} alt="удалить"/>
-    </div>
-  </div>
-);
+
+class Item extends React.Component {
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  };
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  };
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  };
+
+  componentWillMount() {
+    console.log('componentWillMount');
+  };
+
+  componentDidCatch() {
+    console.log('componentDidCatch');
+  };
+
+  getDerivedStateFromError() {
+    console.log('getDerivedStateFromError');
+  };
+
+  render() {
+    const { value, isDone, id, onClickDone, onClickDelete } = this.props;
+
+    return (
+      <div className={
+        classnames({
+          [styles.item]: true,
+          [styles.done]: isDone,
+        })
+      }>
+        <label>
+          <input
+            type="checkbox"
+            name="itemList"
+            onClick={() => onClickDone(id)}
+          />
+          <span className={styles.check}></span>
+          {value}
+        </label>
+        <div className={styles.delete} onClick={() => onClickDelete(id)}>
+          <img src={del} alt="удалить" />
+        </div>
+      </div>
+    );
+  }
+}
 
 Item.defaultProps = {
   isDone: false,
